b824959b19e74282f5d5c35560aca5071403a0d2
Manuel Hintermayr <manuel@hintermayr.net>
2023-07-06 13:33:16 +0200

    (+) finished excersice 8

--- client/tests/test-3.spec.ts
+++ client/tests/test-3.spec.ts
@@ -1,7 +1,15 @@
 import { test, expect } from '@playwright/test';
 
+
+
 test('test', async ({ page }) => {
+  await page.goto('http://localhost:8081/reset');
   await page.goto('http://localhost:8080/#/songs');
+  await page.getByRole('textbox', { name: 'Search by song title, artist, album, or genre' }).click();
+  await page.getByRole('textbox', { name: 'Search by song title, artist, album, or genre' }).fill('Cooler Song');
+  await page.getByRole('textbox', { name: 'Search by song title, artist, album, or genre' }).press('Enter');
+  const bodyContent = await page.textContent('body');
+  expect(bodyContent.includes('No slot content defined.'), "no song was found");
   await page.getByRole('link', { name: 'add' }).click();
   await page.getByLabel('Title').click();
   await page.getByLabel('Title').fill('Cooler Song');
@@ -18,9 +26,14 @@ test('test', async ({ page }) => {
   await page.getByLabel('Album Image Url').fill('www.google.com/logo.png');
   await page.getByLabel('YouTube ID').click();
   await page.getByLabel('YouTube ID').fill('abc1234');
-  await page.getByLabel('Tabas').click();
+  await page.getByLabel('Tab').click();
   await page.getByLabel('Tab').fill('Tab');
   await page.getByLabel('Tab').press('Tab');
   await page.getByLabel('Lyrics').fill('Ulalalalalalala');
   await page.getByRole('button', { name: 'Create Song' }).click();
+  await page.getByRole('textbox', { name: 'Search by song title, artist, album, or genre' }).click();
+  await page.getByRole('textbox', { name: 'Search by song title, artist, album, or genre' }).fill('Cooler Song');
+  await page.getByRole('textbox', { name: 'Search by song title, artist, album, or genre' }).press('Enter');
+  const bodyContentAfter = await page.locator('#app > main > div > div > div > div.white.elevation-2.mt-2 > div').textContent();
+  expect(bodyContentAfter.includes('Cooler Song'), "song was found");
 });
