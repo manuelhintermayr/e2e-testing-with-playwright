3bba13610f13e1141cdedd5160f949629bbcace8
Manuel Hintermayr <manuel@hintermayr.net>
2023-09-11 14:28:31 +0200

    (c) enabled to show html report always

--- client/playwright.config.ts
+++ client/playwright.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['html'], ['list']],
+  reporter: [['html', {open: 'always'}], ['list']],
   // Each test is given 30 seconds.
   timeout: 30000,
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -32,9 +32,9 @@ export default defineConfig({
     trace: 'on-first-retry',
     headless: false,
     video: 'retain-on-failure',
-    launchOptions: {
-      slowMo: 2000,
-    }
+    // launchOptions: {
+    //   slowMo: 2000,
+    // }
   },
   
 
